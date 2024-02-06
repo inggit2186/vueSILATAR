aff93569bf2b4db461c3ba494b253e058f28a634
inggit2186 <inggit.agrinda.1802216@students.um.ac.id>
2024-02-06 07:04:23 +0700

    Fix Rekap Kinerja

--- src/views/pages/adminpanel/RekapKinerja.vue
+++ src/views/pages/adminpanel/RekapKinerja.vue
@@ -59,6 +59,7 @@
                                                 </td>
                                                 <td>
                                                     <BButton v-if="!loadingaksi[item.id] && item.status != 'NONE'" pill size="sm" variant="outline-primary" @click.prevent="aksiStatus(item.id,item.filename)"><b><i-mdi-call-to-action /> AKSI</b></BButton>
+                                                    <span v-else-if="item.status == 'NONE'"><i-guidance-forbidden /></span>
                                                     <BButton v-else pill size="sm" variant="outline-primary"><b> <i-svg-spinners-bars-scale/> Loading...</b></BButton>
                                                 </td>
                                             </tr>
